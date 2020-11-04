cb1c37cf9fd960b64f763a195b7770c4f88eb937
ali26-dev <mohammad.ali26@pm.me>
2020-11-05 03:58:49 +0500

    list view

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,12 @@
+import { Task } from './task';
+
+export const TASKS : Task[] = [
+    { id: 11, name: 'Complete FCC'  },
+    { id: 12, name: 'Cs50 Harvard'  },
+    { id: 13, name: 'Python'  },
+    { id: 14, name: 'Django'  },
+    { id: 15, name: 'React'  },
+    { id: 16, name: 'Ionic'  },
+    { id: 17, name: 'Remote Job'  }
+
+];
